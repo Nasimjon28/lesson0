84239c492ee59e6cbcd777012ff8ef3afe641080
Nasimjon28 <shahlo.reg@gmail.com>
2024-07-08 15:06:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#1st program
+print((9 ** 0.5) * 5)
+#2nd program
+print(1000.1 != 1000 and 9.99 > 9.98)
+#3rd program
+print(1234 // 10 % 100 + 5678 // 10 % 100)
+#4th program
+print(int(13.42) == 42.13 * 100 % 100 or int(42.13) == 13.42 * 100 % 100)
